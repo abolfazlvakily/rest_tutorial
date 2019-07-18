b155e66ae54390e7987bc98dadeac5c290b38bf1
abolfazl <abolfazlvakily@gmail.com>
2019-07-18 08:06:17 -0400

    added REST_FRAMEWORK & AUTHENTICATION_BACKENDS on seeings.py

--- rest_test/settings.py
+++ rest_test/settings.py
@@ -23,3 +23,15 @@ TEMPLATES += [
         },
     }
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        # 'oauth2_provider.ext.rest_framework.OAuth2Authentication',  # django-oauth-toolkit < 1.0.0
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  # django-oauth-toolkit >= 1.0.0
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
+    ),
+}
+AUTHENTICATION_BACKENDS = (
+   'rest_framework_social_oauth2.backends.DjangoOAuth2',
+   'django.contrib.auth.backends.ModelBackend',
+)
