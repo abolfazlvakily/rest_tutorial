5a67fad86e840a1d2e721e654538e6b7941c2efb
abolfazl <abolfazlvakily@gmail.com>
2019-07-20 12:50:58 -0400

    تاابنجا یه لحظه rest رو متوقف کردم و رفتم رو  اپ django_tutorial

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+# from __future__ import unicode_literals
+# from django.db import models
+# from django.contrib.auth.models import User
+#
+# class Profile(models.Model):
+#     name = models.OneToOneField(User, on_delete=models.CASCADE)
 
-# Create your models here.
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.shortcuts import render
+# from rest_framework import generics
+# from django.contrib.auth.models import User
+# from rest_framework.response import Response
+#
+# class Userlist(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+# class CurrentUser(generics.RetrieveAPIView):
+#     def get(self, request):
+#         serializer = UserSerializer(request.user)
+#         return Response(serializer.data)
