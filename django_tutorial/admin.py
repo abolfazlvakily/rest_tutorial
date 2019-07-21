473af8e59fae6c2614858b5d0170d1c36ac07aa9
abolfazl <abolfazlvakily@gmail.com>
2019-07-21 08:17:38 -0400

    django query from db (session_first)

--- django_tutorial/admin.py
+++ django_tutorial/admin.py
@@ -1,2 +1,4 @@
 from django.contrib import admin
-
+from django_tutorial import models
+admin.site.register(models.Question)
+admin.site.register(models.Choice)
--- django_tutorial/models.py
+++ django_tutorial/models.py
@@ -2,7 +2,11 @@ from django.db import models
 class Question(models.Model):
     text = models.CharField(max_length=300)
     tarikh = models.DateTimeField()
+    def __str__(self):
+        return self.text
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=300)
     vote = models.IntegerField(default=0)
+    def __str__(self):
+        return self.text
